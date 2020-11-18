19964039579b33f5c8ef72bc06dc322c0d84800d
sinamna <sina3050@gmail.com>
2020-11-18 19:21:44 +0330

    added hash static method

--- Blockchain.py
+++ Blockchain.py
@@ -1,5 +1,8 @@
 from uuid import uuid4
-
+from Crypto.Hash import SHA256
+from hashlib import sha256
+import json
+import time
 
 class Blockchain:
 
@@ -8,6 +11,8 @@ class Blockchain:
         self.chain = []
         self.nodes = set()
         self.node_id = str(uuid4()).replace('-', ' ')
+
+        #creating genesis block
 
     def register_node(self, node_url):
         pass
@@ -18,17 +23,33 @@ class Blockchain:
     def submit_transaction(self, sender_address, receiver_address, value, signature):
         pass
 
-    def create_block(self, nonce, previous_hash):
+    def create_block(self, nonce, previous_hash=None):
+        """
+
+        :param nonce: <int> the nonce found by the Proof of Word algorithm
+        :param previous_hash: <str> the hash of the previous block (none for genesis block)
+        :return: <dict> the newly created block
+        """
         pass
 
-    def hash(self, block):
-        pass
+    @staticmethod
+    def hash(block):
+        """
+        :param block: <dict> block
+        :return: <str> the SHA-256 hash of block
+        """
+        block_str=json.dump(block,sort_keys=True).encode()
+        return sha256(block_str).hexdigest()
 
     def proof_of_work(self):
         pass
 
     def valid_chain(self):
         pass
+
+    @property
+    def last_block(self):
+        return self.chain[-1]
 
     def resolve_conflicts(self):
         pass
