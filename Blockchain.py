2b41695d53b59556c92797a8b6b07701b6990f82
sinamna <sina3050@gmail.com>
2020-11-27 22:23:39 +0330

    added argument parser for setting flask app running port

--- Blockchain.py
+++ Blockchain.py
@@ -160,3 +160,12 @@ CORS(app)
 # resolving conflicts
 
 #getting nodes
+
+if __name__=='__main__':
+    from argparse import ArgumentParser
+    parser = ArgumentParser()
+    parser.add_argument('-p','--port',default=5000,type=int,help='listening port')
+    args=parser.parse_args()
+    port=args.port
+
+    app.run(host=git '127.0.0.1', port=port)
