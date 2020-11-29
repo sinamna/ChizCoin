ce3e5e50b63428c0141c7df97a18c637afa0a8dc
sinamna <sina3050@gmail.com>
2020-11-29 22:30:47 +0330

    handled basic routes

--- blockchain_client/blockchain_client.py
+++ blockchain_client/blockchain_client.py
@@ -4,8 +4,14 @@ from flask import Flask,jsonify,request,render_template
 
 app=Flask(__name__)
 #index page
+@app.route('/')
+def index():
+    return render_template('./index.html')
 
 #make_transaction_page
+@app.route('/make/transactions')
+def make_transactions():
+    return render_template('./make_transactions.html')
 
 #view_transactions
 
