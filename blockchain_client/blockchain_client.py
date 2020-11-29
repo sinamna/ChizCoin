f5418dc0c5e4a1973caa76ba795702b94b7baae6
sinamna <sina3050@gmail.com>
2020-11-29 22:35:03 +0330

    added create wallet endpoint

--- blockchain_client/blockchain_client.py
+++ blockchain_client/blockchain_client.py
@@ -3,19 +3,30 @@ import Wallet
 from flask import Flask,jsonify,request,render_template
 
 app=Flask(__name__)
+#client can have multiple wallets
+client_wallets=[]
 #index page
 @app.route('/')
 def index():
     return render_template('./index.html')
 
 #make_transaction_page
-@app.route('/make/transactions')
+@app.route('/transactions/make')
 def make_transactions():
     return render_template('./make_transactions.html')
 
 #view_transactions
+@app.route('/transactions/view')
+def view_transactions():
+    return render_template('./view_transactions.html')
 
 #creating wallet
+app.route('/wallet/new',methods=['GET'])
+def create_wallet():
+    wallet=Wallet()
+    response=wallet.to_dict()
+    client_wallets.append(wallet)
+    return jsonify(response),201
 
 #generating transacion
 
