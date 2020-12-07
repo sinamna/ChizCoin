39302ac35308a32d6ebb21fc6f1c601da0681ead
sinamna <sina3050@gmail.com>
2020-12-07 23:02:52 +0330

    added alert

--- blockchain_client/static/js/generate_wallet.js
+++ blockchain_client/static/js/generate_wallet.js
@@ -4,9 +4,9 @@ $(document).ready(()=>{
             url: "/wallet/new",
             type: "GET",
             success:(response)=>{
-                document.getElementById("private-key").innerHTML=response['private_key'];
-                document.getElementById("public-key").innerHTML=response['public_key'];
-                document.getElementById("warning").style.display=block;
+                document.getElementById("private_key").innerHTML=response['private_key'];
+                document.getElementById("public_key").innerHTML=response['public_key'];
+                document.getElementById("warning").style.display="block";
             },
             error:(error)=>{
                 console.log(error)
