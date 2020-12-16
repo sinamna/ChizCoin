5133806994104f96ff1c43189f0f806c02b11a0c
sinamna <sina3050@gmail.com>
2020-12-16 21:49:47 +0330

    done view_trx page in client

---
+++ blockchain_client/static/js/view_trxs.js
@@ -0,0 +1,51 @@
+$(document).ready(()=>{
+    $("#view-transactions-btn").click(()=>{
+        node_url=document.getElementById("node_url").value;
+        $.ajax({
+            url:node_url+"chain",
+            type:'GET',
+            success:response=>{
+                let transactions=[];
+                index=1;
+                const dateOptions={
+                    year:"numeric",
+                    month:"short",
+                    day: "numeric",
+                    hour: "2-digit",
+                    minute: "2-digit"
+                };
+                for(i=1;i<response.length;i++){
+                    let chain =response.chain;
+                    for(j=0;j<chain[i].transactions.length;j++){
+                        let currentDate=new Date(chain[i].transactions.timestamp*1000);
+                        let formattedDate=date.toLocaleTimeString("en-us",options);
+                        const trx=chain[i].transactions[j];
+                        transactions.push([
+                                index,
+                                trx["receiver_address"],
+                                trx["sender_address"],
+                                trx["amount"],
+                                formattedDate,
+                                chain[i]["index"]
+                            ]
+                        );
+                        index++;
+                    };
+                };
+                $("#transactions_table").dataTable({
+                    data:transactions,
+                    columns:[{title:"#"},
+                            {title:"Receiver Address"},
+                            {title:"Sender Address"},
+                            {title:"Chiz Amount"},
+                            {title:"timestamp"},
+                            {title:"Block Id"}],
+                        columnDefs: [ {targets: [1,2,3,4,5], render: $.fn.dataTable.render.ellipsis( 25 )}]
+                    } );
+            },
+            error:error=>{
+                console.log(error);
+            }
+        });
+    });
+});
--- blockchain_node/static/js/current_trx.js
+++ blockchain_node/static/js/current_trx.js
@@ -7,16 +7,16 @@ $(document).ready(()=>{
         success:(response)=>{
             let transactions=[];
             index=1;
+            const dateOptions={
+                year:"numeric",
+                month:"short",
+                day: "numeric",
+                hour: "2-digit",
+                minute: "2-digit"
+            };
             for(i=1;i<response.length;i++){
                 let chain =response.chain;
                 for(j=0;j<chain[i].transactions.length;j++){
-                    const dateOptions={
-                        year:"numeric",
-                        month:"short",
-                        day: "numeric",
-                        hour: "2-digit",
-                        minute: "2-digit"
-                    };
                     let currentDate=new Date(chain[i].transactions.timestamp*1000);
                     let formattedDate=date.toLocaleTimeString("en-us",options);
                     const trx=chain[i].transactions[j];
@@ -63,9 +63,5 @@ $(document).ready(()=>{
             }
         })
     })
-
-
-
-
 
 });
